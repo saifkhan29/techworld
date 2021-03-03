18c1c1bebb6684b4c9771ce33699b184008b32e4
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2021-03-03 14:52:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const links = document.querySelectorAll('.nav-links li');
 icon.addEventListener('click', () => {
     navLinks.classList.toggle('open');
 })
+
+const button = document.querySelector('#tech');
+
+button.addEventListener('click', function () {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+    
+})
+
+const randomColor = function (){
+    const r = Math.floor(Math.random () * 256);
+    const g = Math.floor(Math.random () * 256);
+    const b = Math.floor(Math.random () * 256);
+
+    return `rgb(${r} , ${g} , ${b})`;
+}
